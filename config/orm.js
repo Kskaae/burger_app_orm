ed0bab13f2b7cc0b163bbedb4c3cd256ea5c4a04
Kristopher Kaae <kskaae@gmail.com>
2020-11-13 16:43:35 -0700

    added functional code

--- config/orm.js
+++ config/orm.js
@@ -30,7 +30,7 @@ function objToSql(ob) {
         value = "'" + value + "'";
       }
       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
+      // e.g. {devoured: true} => ["devoured=true"]
       arr.push(key + "=" + value);
     }
   }
@@ -70,7 +70,7 @@ const orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  // An example of objColVals would be {name: panther, devoured: true}
   update: function(table, objColVals, condition, cb) {
     let queryString = "UPDATE " + table;
 
@@ -103,5 +103,5 @@ const orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
